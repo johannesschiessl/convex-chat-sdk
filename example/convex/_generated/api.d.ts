df90561f66242ba2e3f125a11e856f93d6d5b67e
Johannes <150372753+johannesschiessl@users.noreply.github.com>
2026-02-28 00:54:00 +0100

    update convex generated

--- example/convex/_generated/api.d.ts
+++ example/convex/_generated/api.d.ts
@@ -8,13 +8,19 @@
  * @module
  */
 
+import type * as chat from "../chat.js";
+import type * as http from "../http.js";
+
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
 
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  chat: typeof chat;
+  http: typeof http;
+}>;
 
 /**
  * A utility for referencing Convex functions in your app's public API.
