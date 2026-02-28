74d50a1471d76137b335aa067bef9ce976977768
Johannes <150372753+johannesschiessl@users.noreply.github.com>
2026-02-28 11:30:47 +0100

    rename cron

--- src/component/crons.ts
+++ src/component/crons.ts
@@ -4,7 +4,7 @@ import { internal } from "./_generated/api";
 const crons = cronJobs();
 
 crons.daily(
-  "clean up expired chat data",
+  "clean up expired",
   { hourUTC: 4, minuteUTC: 0 },
   internal.lib.cleanupExpired,
   {},
