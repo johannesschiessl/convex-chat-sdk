3d28d56e1d2f75bc4dae94d933bd60f44e10bc81
Johannes <150372753+johannesschiessl@users.noreply.github.com>
2026-02-28 11:20:53 +0100

    update examples

--- example/convex/http.ts
+++ example/convex/http.ts
@@ -1,9 +1,9 @@
-import { registerWebhooks } from "convex-chat-sdk";
+import { registerChatSdkWebhooks } from "convex-chat-sdk";
 import { httpRouter } from "convex/server";
 import { createBot } from "./bot";
 
 const http = httpRouter();
 
-registerWebhooks(http, createBot, { path: "webhooks" });
+registerChatSdkWebhooks(http, createBot, { path: "webhooks" });
 
 export default http;
